6972334e6de3dcef3d813b80ffadfdc938600f9b
Pasan24 <pramudithapasan076@gmail.com>
2025-03-14 08:38:24 +0530

    Created three car object and test the system.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello, World kohomada!");
+        CarRentalSystem rentalSystem = new CarRentalSystem();
+
+        Car car1 = new Car("C001", "Toyota", "Corolla", 3000.0);
+        Car car2 = new Car("C002", "Honda", "Civic", 4000.0);
+        Car car3 = new Car("C003", "Alto", "Celario", 3500.0);
+        rentalSystem.addCar(car1);
+        rentalSystem.addCar(car2);
+        rentalSystem.addCar(car3);
+
+        rentalSystem.menu();
+    
     }
 }
